e2d677cf698b821145e2b05c6d1d48ed836f09d7
Senthil Kumaran <senthilkumaran.rajan@syncfusion.com>
2020-04-16 21:17:17 +0530

    Getting started sample modified

---
+++ ComboBox/Model/PopulationInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedComboBox
+{
+    public class PopulationInfo
+    {
+        private string continent;
+        private double population;
+        private string country;
+        private double growth;
+
+        public string Continent
+        {
+            get { return continent; }
+            set { continent = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public double Growth
+        {
+            get { return growth; }
+            set { growth = value; }
+        }
+
+        public double Population
+        {
+            get { return population; }
+            set { population = value; }
+        }
+    }
+}
---
+++ ComboBox/ViewModel/PopulationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedComboBox
+{
+    public class PopulationViewModel
+    {
+        public PopulationViewModel()
+        {
+            this.PopulationDetails = new ObservableCollection<PopulationInfo>();
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Asia", Country = "Indonesia", Growth = 3, Population = 237641326 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Asia", Country = "Russia", Growth = 2, Population = 152518015 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Asia", Country = "Malaysia", Growth = 1, Population = 29672000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "North America", Country = "United States", Growth = 4, Population = 315645000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "North America", Country = "Mexico", Growth = 2, Population = 112336538 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "North America", Country = "Canada", Growth = 1, Population = 35056064 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "South America", Country = "Colombia", Growth = 1, Population = 47000000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "South America", Country = "Brazil", Growth = 3, Population = 193946886 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Africa", Country = "Nigeria", Growth = 2, Population = 170901000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Africa", Country = "Egypt", Growth = 1, Population = 83661000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Europe", Country = "Germany", Growth = 1, Population = 81993000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Europe", Country = "France", Growth = 1, Population = 65605000 });
+            PopulationDetails.Add(new PopulationInfo() { Continent = "Europe", Country = "UK", Growth = 1, Population = 63181775 });
+        }
+
+        public ObservableCollection<PopulationInfo> PopulationDetails
+        {
+            get;
+            set;
+        }
+    }
+}
